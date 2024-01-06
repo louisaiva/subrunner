9b1282b2762450d84ed147f942afc05ad8504b56
louisaiva <deltasfer@gmail.com>
2024-01-06 15:25:36 +0100

    (#68.2) Chest and InteractChest

--- Assets/Scripts/Environment/Chest.cs
+++ Assets/Scripts/Environment/Chest.cs
@@ -1,38 +1,29 @@
 using UnityEngine;
-using System.Collections.Generic;
+using System.Collections;
 
 
 [RequireComponent(typeof(AnimationHandler))]
-public class Chest : MonoBehaviour, I_Interactable
+public class Chest : MonoBehaviour
 {
 
-    // la classe CHEST sert à créer des coffres
-    // peut contenir des objets physiques
-    // pas obligatoirement alignée avec les tiles
-
-    // mettre un champ de force qui attire les objets vers le coffre,
-    // comme ça on peut juste drop les objets à côté du coffre
-
     // ANIMATION
-    private AnimationHandler anim_handler;
-    private ChestAnims anims = new ChestAnims();
+    protected AnimationHandler anim_handler;
+    protected ChestAnims anims = new ChestAnims();
 
-    // OPENING
-    public bool is_open = false;
-    private float openin_time = 1f;
+    [Header("OPENING")]
+    [SerializeField] protected bool is_open = false;
+    [SerializeField] protected bool is_moving = false;
+    [SerializeField] protected float openin_duration = 0.5f;
 
+ 
     // inventory
     public Inventory inventory;
 
-    // interactions
-    public bool is_interacting { get; set; } // est en train d'interagir
-    
     // unity functions
     void Awake()
     {
         // on récupère l'animation handler
         anim_handler = GetComponent<AnimationHandler>();
-        // anim_handler.debug = true;
 
         // on récupère l'inventaire
         inventory = transform.Find("inventory").GetComponent<Inventory>();
@@ -41,71 +32,72 @@ public class Chest : MonoBehaviour, I_Interactable
         inventory.randomize();
     }
 
+    protected virtual void Events() {}
+
     void Update()
     {
         // on check les events
         Events();
-
-        // on met à jour les animations
+        
+        // on met à jour l'inventaire
         if (!is_open)
         {
-            // on met à jour l'inventaire
             inventory.setShow(false);
-
-            if (!is_interacting)
-            {
-                // on regarde si on a fini l'animation
-                if (anim_handler.IsForcing()) { return; }
-
-                // on met à jour les animations
-                anim_handler.ChangeAnim(anims.idle_closed);
-            }
         }
     }
 
-    void Events()
-    {
-        // print("chest : " + is_open + " " + is_interacting);
-        // on regarde si on appuie sur la touche d'interaction (E)
-        // lorsqu'on est déjà ouvert
-        if (Input.GetButtonDown("interact") && is_open)
-        {
-            // on transvase tous les objets dans l'inventaire du perso
-            foreach (Item item in inventory.getItems())
-            {
-                // on ajoute l'item au perso
-                // perso.GetComponent<Inventory>().addItem(item);
-                inventory.dropItem(item);
-            }
-        }
-    }
 
     // MAIN FUNCTIONS
-    public void open()
+    protected void open()
     {
-        print("open chest");
-        is_open = true;
+        CancelInvoke();
 
-        // on met à jour les animations
-        anim_handler.StopForcing();
+        // on joue l'animation
+        anim_handler.ChangeAnim(anims.openin, openin_duration);
+
+        // on ouvre le coffre
+        Invoke("success_open", openin_duration);
+
+        is_moving = true;
+    }
+
+    protected void success_open()
+    {
+        // on ouvre le coffre
+        is_open = true;
+        is_moving = false;
+
+        // on joue l'animation
         anim_handler.ChangeAnim(anims.idle_open);
 
         // on met à jour l'inventaire
         inventory.setShow(true);
     }
 
-    public void close()
+    protected void close()
     {
-        print("close chest");
+        CancelInvoke();
 
-        // on arrête de forcer l'animation -> pour être sûr qu'on puisse fermer le coffre
-        anim_handler.StopForcing();
+        // on met à jour l'inventaire
+        inventory.setShow(false);
 
-        // on met à jour les animations
-        anim_handler.ChangeAnimTilEnd(anims.closin);
+        // on ferme le coffre
+        is_moving = true;
+
+        // on joue l'animation
+        anim_handler.ChangeAnim(anims.closin, openin_duration);
+        Invoke("success_close", openin_duration);
+        
+    }
+
+    protected void success_close()
+    {
+        // on joue l'animation
+        anim_handler.ChangeAnim(anims.idle_closed);
 
         // on ferme le coffre
         is_open = false;
+        is_moving = false;
     }
 
 
@@ -121,42 +113,8 @@ public class Chest : MonoBehaviour, I_Interactable
         inventory.forceAddItem(item);
     }
 
-    // INTERACTIONS
-    public bool isInteractable()
-    {
-        return !is_interacting;
-    }
-
-    public void interact()
-    {
-        // on met à jour les animations
-        if (!anim_handler.ChangeAnimTilEnd(anims.openin, openin_time)) { return; }
-
-        print("interact chest");
-
-        // interaction
-        is_interacting = true;
-
-        // on ouvre le coffre dans openin_time
-        Invoke("open", openin_time);
-    }
-
-    public void stopInteract()
-    {
-        print("stop interact chest");
-
-        if (is_open) { close(); }
-
-        if (is_interacting)
-        {
-            // on arrête l'invocation de "open"
-            CancelInvoke("open");
-
-            // on arrête l'interaction
-            is_interacting = false;
-        }
-    }
 }
+
 
 public class ChestAnims
 {
@@ -166,6 +124,5 @@ public class ChestAnims
     public string idle_closed = "chest_idle_closed";
     public string openin = "chest_openin";
     public string closin = "chest_closin";
-    // public string hackin = "chest_hacked";
 
 }
---
+++ Assets/Scripts/Environment/InteractChest.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(AnimationHandler))]
+public class InteractChest : Chest, I_Interactable
+{
+
+    public bool is_interacting { get; set; } // est en train d'interagir
+
+    // EVENTS
+    protected override void Events()
+    {
+        // on regarde si on appuie sur la touche d'interaction (E)
+        // lorsqu'on est déjà ouvert
+        if (is_open && !is_moving && Input.GetButtonDown("interact"))
+        {
+            // on transvase tous les objets dans l'inventaire du perso
+            foreach (Item item in inventory.getItems())
+            {
+                // on ajoute l'item au perso
+                inventory.dropItem(item);
+            }
+        }
+    }
+
+    // INTERACTIONS
+    public bool isInteractable()
+    {
+        return !is_interacting;
+    }
+
+    public void interact()
+    {
+        print("interact with "+gameObject.name);
+
+        // interaction
+        is_interacting = true;
+        
+        open();
+    }
+
+    public void stopInteract()
+    {
+        print("stop interact with "+gameObject.name);
+        
+        close();
+
+        // on arrête l'interaction
+        is_interacting = false;
+    }
+
+}
