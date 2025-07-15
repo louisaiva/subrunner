7ec55b488ba6011ab8b748d53b64e4d7e6e753d6
louis aiva <deltasfer@gmail.com>
2025-07-15 03:32:39 +0200

    (#160.6) WanderTargetSensor gives a position on NavMesh

--- Assets/Scripts/GOAP/Sensors/WanderTargetSensor.cs
+++ Assets/Scripts/GOAP/Sensors/WanderTargetSensor.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using Pathfinding;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEngine.AI;
 
 namespace subrunner.goap
 {
@@ -21,28 +22,15 @@ namespace subrunner.goap
 
         public override ITarget Sense(IActionReceiver agent, IComponentReference references, ITarget existingTarget)
         {
-            // gets the agent's current node
-            GraphNode agent_node = AstarPath.active.GetNearest(agent.Transform.position, NNConstraint.None).node;
-
             // get the ia & exploration range
             IA ia = references.GetCachedComponentInParent<IA>();
 
             // find a random position to go
-            Vector3 random_position = existingTarget is PositionTarget positionTarget ? positionTarget.Position : Vector3.zero;
-            for (int i = 0; i < 5; i++)
+            Vector3 random_position = getRandomPositionInRangeNavMesh(agent.Transform.position, ia.exploration_radius);
+            if (random_position == default)
             {
-                // if the position is not valid, we get a new random position
-                GraphNode random_node = getRandomNodeInRange(agent.Transform.position, ia.exploration_radius);
-
-                // we check if the position is reachable
-                random_position = (Vector3) random_node.position;
-                if (isReachablePosition(agent_node, random_node)) { break; }
-
-                if (i == 4)
-                {
-                    if (existingTarget is PositionTarget) { return existingTarget as PositionTarget; }
-                    return null;
-                }
+                if (existingTarget is PositionTarget) { return existingTarget as PositionTarget; }
+                return null;
             }
 
             // the position is valid, we set the z as 0 for 2D gameplay
@@ -56,10 +44,30 @@ namespace subrunner.goap
                 existingTargetPosition.SetPosition(random_position);
                 return existingTarget;
             }
-            
+
             return new PositionTarget(random_position);
         }
 
+
+        // ASTAR PROJECT
+        private Vector3 getRandomPositionInRangeAstar(Vector2 center, float range)
+        {
+            Vector3 position;
+            // gets the agent's current node
+            GraphNode agent_node = AstarPath.active.GetNearest(center, NNConstraint.None).node;
+
+            for (int i = 0; i < 5; i++)
+            {
+                // if the position is not valid, we get a new random position
+                GraphNode random_node = getRandomNodeInRange(center, range);
+
+                // we check if the position is reachable
+                position = (Vector3)random_node.position;
+                if (isReachablePosition(agent_node, random_node)) { return position; }
+            }
+
+            return default;
+        }
         private GraphNode getRandomNodeInRange(Vector2 center, float range)
         {
             // generates a random position in a circle around the center
@@ -106,5 +114,55 @@ namespace subrunner.goap
             Debug.LogWarning("(IdleTargetSensor) No walkable node found");
             return null;
         } */
+
+
+        // NAV MESH
+        private Vector3 getRandomPositionInRangeNavMesh(Vector2 center, float range)
+        {
+            int attempts = 10;
+            for (int i = 0; i < attempts; i++)
+            {
+                Vector3 randomPosition = getRandomPositionOnNavMesh(center, range);
+                if (randomPosition == default) { continue; }
+
+                // checks if the destination is reachable
+                NavMeshPath path = new NavMeshPath();
+                if (!NavMesh.CalculatePath(center, randomPosition, NavMesh.AllAreas, path)) { continue; }
+
+                // on le clamp pour faire en sorte que l'ia ne puisse parcourir que range de distance maximale
+                return getMaxDistanceAlongPath(path, range);
+            }
+
+            Debug.LogWarning("(IdleTargetSensor) No walkable position found on the nav mesh");
+            return default;
+        }
+        private Vector3 getRandomPositionOnNavMesh(Vector2 center, float range)
+        {
+            // pick a random position on the nav mesh
+            Vector3 randomPosition = (Vector3) center + Random.insideUnitSphere * range;
+
+            NavMeshHit hit;
+            if (NavMesh.SamplePosition(randomPosition, out hit, 10f, NavMesh.AllAreas))
+            {
+                return hit.position;
+            }
+
+            return default;
+        }
+        private Vector3 getMaxDistanceAlongPath(NavMeshPath path, float max_distance)
+        {
+            // we go through all corners and calculate the distance
+            float distance = 0f;
+            for (int i = 0; i < path.corners.Length - 1; i++)
+            {
+                distance += Vector3.Distance(path.corners[i], path.corners[i + 1]);
+                if (distance > max_distance)
+                {
+                    // we return the position of the corner that is at max_distance
+                    return path.corners[i + 1];
+                }
+            }
+            return path.corners.Last();
+        }
     }
 }
