45d4fe891908aeabc22016005ad281b0bc6c3a95
louisaiva <deltasfer@gmail.com>
2026-05-28 15:52:57 +0200

    (#311.2) UI_HeartIcon

---
+++ Assets/Scripts/UI/Pools/HUD/UI_HeartIcon.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UI_HeartIcon : MonoBehaviour
+{
+    [SerializeField] private List<HeartSprite> heart_sprites; // from dead to full life
+    [SerializeField] private Image heart_image;
+
+    private void Update()
+    {
+        if (Controller.Perso == null) { return; }
+        if (!Controller.Perso.TryGetCapacity<HealthCapacity>(out var health_capacity)) { return; }
+
+        // on récupère le pourcentage de vie actuel du perso
+        float life_percentage = health_capacity.LifePourcent;
+
+        // on trouve le sprite correspondant dans la liste
+        foreach (HeartSprite heart_sprite in heart_sprites)
+        {
+            if (life_percentage >= heart_sprite.life_percentage_threshold)
+            {
+                heart_image.sprite = heart_sprite.sprite;
+                break;
+            }
+        }
+    }
+}
+
+[Serializable] public class HeartSprite
+{
+    public Sprite sprite;
+    public float life_percentage_threshold;
+}
